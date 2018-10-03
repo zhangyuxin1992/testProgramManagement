578d5d6bdee7f69ff797b2f3d0604d330b744c53
zhangyuxin <zyxsichuan@sina.com>
2018-10-03 20:23:39 +0800

    version 3.0

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test
 	public static void main(String[] args){
 		System.out.println("hello");
 		System.out.println("create a branch dev");
+		System.out.println("dev program 1.0");
 	}
 }
